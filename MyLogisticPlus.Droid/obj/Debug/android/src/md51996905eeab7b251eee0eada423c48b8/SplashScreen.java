6822eb0fcb943b21b9947077a0408ef7c22d17fb
Yaroslav Yuzyk <yaroslav.yuzyk@gmail.com>
2017-01-10 19:52:06 +0200

    Create project.

---
+++ MyLogisticPlus.Droid/obj/Debug/android/src/md51996905eeab7b251eee0eada423c48b8/SplashScreen.java
@@ -0,0 +1,38 @@
+package md51996905eeab7b251eee0eada423c48b8;
+
+
+public class SplashScreen
+	extends mvvmcross.droid.views.MvxSplashScreenActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MyLogisticPlus.Droid.SplashScreen, MyLogisticPlus.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", SplashScreen.class, __md_methods);
+	}
+
+
+	public SplashScreen () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == SplashScreen.class)
+			mono.android.TypeManager.Activate ("MyLogisticPlus.Droid.SplashScreen, MyLogisticPlus.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
